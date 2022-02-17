7cb791bc975dd8f09be2774878aea9e61b43ad02
sh0layere9 <carrera911992@gmail.com>
2022-02-18 02:01:35 +0900

    Added comment

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+// 要素を複製 複製したい要素.cloneNode(真偽値) appendChild等と共に使うこと trueを外すと複製されない
+
+// my-clone-boxを取得
 const cloneBox = document.getElementById('my-clone-box');
 
+// my-clone-boxをクリックすると発火
 cloneBox.addEventListener('click', () =>
 {
+    // 親要素のcontainerを取得
     const container = document.querySelector('.container');
 
+    // my-clone-boxを複製する
     const clone = cloneBox.cloneNode(true);
 
+    // 複製したmy-clone-boxを、containerの末尾に挿入する
     container.appendChild(clone);
 });
 
